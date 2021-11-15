1fc37999fc781722f1ec9126a00eb2a7e0f1ee75
PixelatedLagg <81400034+PixelatedLagg@users.noreply.github.com>
2021-11-15 00:13:47 -0500

    starting webassembly :>

---
+++ src/Impart.Scripting/Initialization.cs
@@ -0,0 +1,10 @@
+namespace Impart.Scripting
+{
+    internal static class Initialize
+    {
+        internal static void Start()
+        {
+            //initialize everything needed to execute code in browser
+        }
+    }
+}
