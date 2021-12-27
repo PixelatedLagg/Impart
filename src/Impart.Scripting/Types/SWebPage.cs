18f6d5835d8964a06bbd9308c3d1b374c4d5f3e8
PixelatedLagg <81400034+PixelatedLagg@users.noreply.github.com>
2021-12-27 14:50:52 -0500

    assembly info

---
+++ src/Impart.Scripting/Types/SWebPage.cs
@@ -0,0 +1,15 @@
+namespace Impart.Scripting
+{
+    public sealed class SWebPage
+    {
+        private string file;
+        public SWebPage(string file)
+        {
+            this.file = file;
+        }
+        public void Render()
+        {
+            
+        }
+    }
+}
