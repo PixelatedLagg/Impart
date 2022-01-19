d8f271e782c1c138e0d6eda79be4348b0507f5ec
PixelatedLagg <81400034+PixelatedLagg@users.noreply.github.com>
2022-01-18 20:59:34 -0500

    optimization + data documentation

--- src/Impart.Data/Letter.cs
+++ src/Impart.Data/Letter.cs
@@ -1,41 +1,51 @@
 namespace Impart.Data
 {
+    /// <summary>Randomly generate a letter.</summary>
     public static class Letter
     {
-        private static string AlphabetLower = "abcdefghijklmnopqrstuvwxyz";
-        private static string AlphabetHigher = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-        private static string AlphabetInvariant = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
-        private static string Numbers = "0123456789";
-        private static string Specials = @"\|!#$%&/()=?»«@£§€{}.-;'<>_,";
-
         /// <summary>Randomly generate an alphabetical character.</summary>
         /// <param name="type">Whether to generate an uppercase or lowercase letter. (Default is set to generate both uppercase and lowercase.)</param>
         public static char Alphabet(bool? type = null)
         {
             if (type == null)
             {
-                return AlphabetInvariant[(int)Impart.Data.Number.Range(0, 50)];
+                return "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"[(int)Data.Number.Range(0, 50)];
             }
             if (type.Value)
             {
-                return AlphabetHigher[(int)Impart.Data.Number.Range(0, 25)];
+                return "ABCDEFGHIJKLMNOPQRSTUVWXYZ"[(int)Data.Number.Range(0, 25)];
             }
             else
             {
-                return AlphabetLower[(int)Impart.Data.Number.Range(0, 25)];
+                return "AlphabetLower"[(int)Data.Number.Range(0, 25)];
             }
         }
 
         /// <summary>Randomly generate an numerical character.</summary>
         public static char Number()
         {
-            return Numbers[(int)Impart.Data.Number.Range(0, 9)];
+            return Data.Number.Range(0, 9).ToString()[0];
         }
 
         /// <summary>Randomly generate a special character.</summary>
         public static char Special()
         {
-            return Specials[(int)Impart.Data.Number.Range(0, 27)];
+            return @"\|!#$%&/()=?»«@£§€{}.-;'<>_,"[(int)Data.Number.Range(0, 27)];
+        }
+
+        /// <summary>Randomly generate any character.</summary>
+        public static char Any()
+        {
+            return @"ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789\|!#$%&/()=?»«@£§€{}.-;'<>_,"[(int)Data.Number.Range(0, 89)];
+        }
+
+        /// <summary>All character types.</summary>
+        public enum Types
+        {
+            Any,
+            Special,
+            Number,
+            Alphabet
         }
     }
 }
--- src/Impart.Data/Number.cs
+++ src/Impart.Data/Number.cs
@@ -1,7 +1,9 @@
 using System;
+using System.Text;
 
 namespace Impart.Data
 {
+    /// <summary>Randomly generate a number.</summary>
     public static class Number
     {
         private const long m = 4294967296;
@@ -16,14 +18,31 @@ namespace Impart.Data
             }
             return _last;
         }
-        public static long Long()
+        private static long Long()
         {
             _last = ((a * Seed()) + c) % m;
             return _last;
         }
+
+        /// <summary>Generate a number in a specific range.</summary>
+        /// <param name="min">The minimum number to generate.</param>
+        /// <param name="max">The maximum number to generate.</param>
         public static long Range(long min, long max)
         {
             return Long() % (max - min) + min;
+        }
+
+        /// <summary>Generate a number with the specified length.</summary>
+        /// <param name="length">The length of the number to generate.</param>
+        public static long Length(int length)
+        {
+            StringBuilder result = new StringBuilder(length);
+            while (length > 0)
+            {
+                length--;
+                result.Append(Data.Number.Range(0, 9));
+            }
+            return Convert.ToInt64(result.ToString());
         }
     }
 }
