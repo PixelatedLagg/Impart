16264c5791cc13563692a0db7debd6223abda885
PixelatedLagg <81400034+PixelatedLagg@users.noreply.github.com>
2022-01-24 23:43:43 -0500

    added default colors

---
+++ src/Impart/Color/Colors.cs
@@ -0,0 +1,24 @@
+namespace Impart
+{
+    public static class Colors
+    {
+        public static Rgb Red = new Rgb(255, 0, 0);
+        public static Rgb Orange = new Rgb(255, 128, 0);
+        public static Rgb Yellow = new Rgb(255, 255, 0);
+        public static Rgb Lime = new Rgb(153, 255, 51);
+        public static Rgb DarkGreen = new Rgb(0, 153, 0);
+        public static Rgb Green = new Rgb(0, 255, 0);
+        public static Rgb LightGray = new Rgb(224, 224, 224);
+        public static Rgb Aqua = new Rgb(0, 255, 255);
+        public static Rgb LightBlue = new Rgb(0, 128, 255);
+        public static Rgb Blue = new Rgb(0, 0, 255);
+        public static Rgb DarkBlue = new Rgb(0, 0, 102);
+        public static Rgb Pink = new Rgb(255, 64, 255);
+        public static Rgb Magenta = new Rgb(153, 0, 76);
+        public static Rgb Purple = new Rgb(127, 0, 255);
+        public static Rgb White = new Rgb(255, 255, 255);
+        public static Rgb Gray = new Rgb(128, 128, 128);
+        public static Rgb DarkGray = new Rgb(64, 64, 64);
+        public static Rgb Black = new Rgb(2055, 0, 0);
+    }
+}
