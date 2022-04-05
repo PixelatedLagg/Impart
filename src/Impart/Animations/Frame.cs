f1f7df9ebdb66ac3476d58371f2ea5a1d4080c49
PixelatedLagg <81400034+PixelatedLagg@users.noreply.github.com>
2022-04-04 23:55:36 -0400

    animations

--- src/Impart/Animations/Frame.cs
+++ src/Impart/Animations/Frame.cs
@@ -1,10 +1,22 @@
 namespace Impart
 {
+    /// <summary>The class for a single Frame part of Animation.</summary>
     public struct Frame
     {
+        /// <value>The position of the Frame.</value>
         public readonly Percent Position;
+
+        /// <value>The type of Change.</value>
         public readonly ChangeType ChangeType;
+
+        /// <value>The change to take place in the Frame.</value>
         public readonly object Change;
+
+        /// <summary>Creates a Frame instance.</summary>
+        /// <returns>A Frame instance.</returns>
+        /// <param name="position">The position value.</param>
+        /// <param name="changeType">The type of Change.</param>
+        /// <param name="change">The change to take place.</param>
         public Frame(Percent position, ChangeType changeType, object change)
         {
             Position = position;
