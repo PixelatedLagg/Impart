418c99146da4a9e38b3037c47972408245a5ae50
PixelatedLagg <81400034+PixelatedLagg@users.noreply.github.com>
2022-05-10 23:58:09 -0400

    add: static colors to hsl and hex

--- src/Impart/Units/Color/Types/Hex.cs
+++ src/Impart/Units/Color/Types/Hex.cs
@@ -1,5 +1,3 @@
-using System;
-
 namespace Impart
 {
     /// <summary>The class for a HEX color.</summary>
@@ -84,5 +82,59 @@ namespace Impart
         /// <summary>Convert the String instance to Hex.</summary>
         /// <param name="s">The String to convert.</param>
         public static implicit operator Hex(string s) => new Hex(s);
+
+        /// <value>Red represented by Hex.</value>
+        public static Hex Red = new Hex("FF0000");
+
+        /// <value>Orange represented by Hex</value>
+        public static Hex Orange = new Hex("FF8000");
+
+        /// <value>Yellow represented by Hex</value>
+        public static Hex Yellow = new Hex("FFFF00");
+
+        /// <value>Lime represented by Hex</value>
+        public static Hex Lime = new Hex("99FF33");
+
+        /// <value>Dark green represented by Hex</value>
+        public static Hex DarkGreen = new Hex("009900");
+
+        /// <value>Green represented by Hex</value>
+        public static Hex Green = new Hex("00FF00");
+
+        /// <value>Light gray represented by Hex</value>
+        public static Hex LightGray = new Hex("E0E0E0");
+
+        /// <value>Aqua represented by Hex</value>
+        public static Hex Aqua = new Hex("00FFFF");
+
+        /// <value>Light blue represented by Hex</value>
+        public static Hex LightBlue = new Hex("0080FF");
+
+        /// <value>Blue represented by Hex</value>
+        public static Hex Blue = new Hex("0000FF");
+
+        /// <value>Dark blue represented by Hex</value>
+        public static Hex DarkBlue = new Hex("000066");
+
+        /// <value>Pink represented by Hex</value>
+        public static Hex Pink = new Hex("FF40FF");
+
+        /// <value>Magenta represented by Hex</value>
+        public static Hex Magenta = new Hex("99004C");
+
+        /// <value>Purple represented by Hex</value>
+        public static Hex Purple = new Hex("7F00FF");
+
+        /// <value>White represented by Hex</value>
+        public static Hex White = new Hex("FFFFFF");
+
+        /// <value>Gray represented by Hex</value>
+        public static Hex Gray = new Hex("808080");
+
+        /// <value>Dark gray represented by Hex</value>
+        public static Hex DarkGray = new Hex("404040");
+
+        /// <value>Black represented by Hex</value>
+        public static Hex Black = new Hex("000000");
     }
 }
