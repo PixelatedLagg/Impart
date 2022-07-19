157e92ad74a2cb28b63b479f97a11ecd9fc080ad
PixelatedLagg <81400034+PixelatedLagg@users.noreply.github.com>
2022-07-18 23:28:32 -0400

    add: font documentation

--- src/Impart/Font/Font.cs
+++ src/Impart/Font/Font.cs
@@ -2,9 +2,15 @@ using System.Text;
 
 namespace Impart
 {
+    /// <summary>A font sourced from file(s).</summary>
     public struct Font
     {
         private string Render;
+
+        /// <summary>Creates a Font instance.</summary>
+        /// <param name="name">The name for the font family.</param>
+        /// <param name="url">The URL to the source file.</param>
+        /// <param name="type">The type of file format.</param>
         public Font(string name, string url, FontType type)
         {
             Render = (type) switch
@@ -17,6 +23,10 @@ namespace Impart
                 _ => $"font-family: \"{name}\"; src: url(\"{url}\") format(\"woff2\");"
             };
         }
+
+        /// <summary>Creates a Font instance.</summary>
+        /// <param name="name">The name for the font family.</param>
+        /// <param name="args">An array of URLs to source files and those files' format types.</param>
         public Font(string name, params (string, FontType)[] args)
         {
             StringBuilder result = new StringBuilder($"font-family: \"{name}\"; src: ");
@@ -35,6 +45,8 @@ namespace Impart
             result.Length--;
             Render = result.Append(';').ToString();
         }
+
+        /// <summary>Returns the instance as a String.</summary>
         public override string ToString()
         {
             return Render;
--- src/Impart/Font/FontType.cs
+++ src/Impart/Font/FontType.cs
@@ -1,12 +1,24 @@
 namespace Impart
 {
+    /// <summary>All the Font format types.</summary>
     public enum FontType
     {
+        /// <value>Web Open Font Format.</value>
         WOFF,
+
+        /// <value>Web Open Font Format 2.</value>
         WOFF2,
+
+        /// <value>TrueType Font.</value>
         TTF,
+
+        /// <value>OpenType Font.</value>
         OTF,
+
+        /// <value>Embedded OpenType Font.</value>
         EOT,
+
+        /// <value>Scalable Vector Graphics.</value>
         SVG
     }
 }
