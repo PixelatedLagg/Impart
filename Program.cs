642048a3ed2d3c91cbb7e4bd2bef31bfd11e5a9f
PixelatedLagg <81400034+PixelatedLagg@users.noreply.github.com>
2022-09-16 23:40:53 -0400

    + color parsing

--- Program.cs
+++ Program.cs
@@ -1,7 +1,12 @@
-﻿class Program
+﻿using Impart.Internal;
+using Impart;
+using System;
+
+class Program
 {
     public static void Main()
     {
-        
+        Hsl hsl = (Hsl)StorageExtensions.GetColor("hsl(0, 100%, 0%)");
+        Console.WriteLine((((float h, float s, float l))hsl));
     }
 }
--- src/Impart.Internal/StorageExtensions.cs
+++ src/Impart.Internal/StorageExtensions.cs
@@ -1,3 +1,5 @@
+using System;
+
 namespace Impart.Internal
 {
     public static class StorageExtensions
@@ -8,12 +10,25 @@ namespace Impart.Internal
             switch (attrType)
             {
                 case "background-color":
-                    return new Attr(AttrType.BackgroundColor);
+                    return new Attr(AttrType.BackgroundColor, GetColor(args[0]));
             }
         }
-        public static ExtAttr GetExtAttr(string extAttrType, string extAttrValue)
+        /*public static ExtAttr GetExtAttr(string extAttrType, string extAttrValue)
         {
-
+        }*/
+        public static Color GetColor(string colorValue)
+        {
+            switch (colorValue[0])
+            {
+                case '#':
+                    return new Hex(colorValue.Remove(0, 1));
+                case 'r':
+                    string[] rbgValues = colorValue.Remove(0, 4).Remove(colorValue.Length - 5, 1).Split(',');
+                    return new Rgb(Int32.Parse(rbgValues[0]), Int32.Parse(rbgValues[1]), Int32.Parse(rbgValues[2]));
+                default:
+                    string[] hslValues = colorValue.Remove(0, 4).Remove(colorValue.Length - 5, 1).Split(',');
+                    return new Hsl(Int32.Parse(hslValues[0]), Int32.Parse(hslValues[1].Remove(hslValues[1].Length - 1, 1)), Int32.Parse(hslValues[2].Remove(hslValues[2].Length - 1, 1)));
+            }
         }
     }
 }
